64c2583a311c7726a97e3087abc9815e5ad2c5af
sangwon0707 <sangwon07@gmail.com>
2024-03-22 14:58:55 +0900

    Orders/controler/service[상원]

--- src/main/java/com/example/demo/dao/OrdersDAO.java
+++ src/main/java/com/example/demo/dao/OrdersDAO.java
@@ -1,12 +1,27 @@
 package com.example.demo.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.demo.entity.Orders;
+
+
+import jakarta.transaction.Transactional;
 
 public interface OrdersDAO extends JpaRepository<Orders, Integer> {
 	@Query(value = "select nvl(max(orderid),0)+1 from orders", 
 			nativeQuery = true)
 	public int getNextNo();
+	
+	@Modifying
+	@Query(value ="insert into orders o(o.orderid,o.custid,o.bookid,o.saleprice,o.orderdate) values(:#{#o.orderid},:#{#o.customer.custid},:#{#o.book.bookid},:#{#o.saleprice},sysdate)",nativeQuery = true)
+	@Transactional
+	public void insert(@Param("o") Orders o);
+	
+	public List<Orders> findByCustomer_NameOrderByOrderid(String name);
+	public List<Orders> findByCustomer_Name(String name);
 }
--- src/main/java/com/example/demo/service/OrdersService.java
+++ src/main/java/com/example/demo/service/OrdersService.java
@@ -1,9 +1,12 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.dao.OrdersDAO;
+import com.example.demo.entity.Orders;
 
 import lombok.Setter;
 
@@ -13,4 +16,22 @@ public class OrdersService {
 	@Autowired
 	private OrdersDAO dao;
 	
+	public int getNextNo(){
+		return dao.getNextNo();
+	}
+	
+	public void insert(Orders o){
+		dao.insert(o);
+	}
+	
+	
+	public List<Orders> findAll(String keyword){
+		if(keyword != null && !keyword.isEmpty()) {
+			return dao.findByCustomer_Name(keyword);
+		}
+		else {
+			return dao.findAll();
+		}
+	}
+
 }
