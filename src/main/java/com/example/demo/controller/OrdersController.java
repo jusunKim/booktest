5cc514e924f64f56c50f4a2b6ef6680765f23340
jusunKim <155524612+jusunKim@users.noreply.github.com>
2024-03-22 16:08:24 +0900

    merge 시도 [주선]

--- src/main/java/com/example/demo/controller/OrdersController.java
+++ src/main/java/com/example/demo/controller/OrdersController.java
@@ -1,47 +1,47 @@
-//package com.example.demo.controller;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//
-//
-//import com.example.demo.entity.Orders;
-//import com.example.demo.service.BookService;
-//import com.example.demo.service.CustomerService;
-//import com.example.demo.service.OrdersService;
-//
-//import lombok.Setter;
-//
-//@Controller
-//@Setter
-//public class OrdersController {
-//	@Autowired
-//	private BookService bs; 
-//	
-//	@Autowired
-//	private CustomerService cs;
-//	 
-//	@Autowired
-//	private OrdersService os; 
-//	
-//	@GetMapping("/orders/list")
-//	public void list(Model model, String keyword) {
-//		model.addAttribute("list", os.findAll(keyword));
-//	}
-//	
-//	@GetMapping("/orders/insert")
-//	public void insert(Model model){
+package com.example.demo.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+import com.example.demo.entity.Orders;
+import com.example.demo.service.BookService;
+import com.example.demo.service.CustomerService;
+import com.example.demo.service.OrdersService; 
+
+import lombok.Setter;
+
+@Controller
+@Setter
+public class OrdersController {
+	@Autowired
+	private BookService bs; 
+	
+	@Autowired
+	private CustomerService cs;
+	 
+	@Autowired
+	private OrdersService os; 
+	
+	@GetMapping("/orders/list")
+	public void list(Model model, String keyword) {
+		model.addAttribute("list", os.findAll(keyword));
+	}
+	
+	@GetMapping("/orders/insert")
+	public void insert(Model model){
 //		model.addAttribute("bList", bs.findAll());
 //		model.addAttribute("cList", cs.findAll());
-//		model.addAttribute("orderid", os.getNextNo());
-//	}
-//	
-//	@PostMapping("/insertOrder")
-//	public String save(Orders o) {
-//		String view = "redirect:/orders/insert";
-//		os.insert(o);
-//		return view;
-//	}
-//	
-//}
+		model.addAttribute("orderid", os.getNextNo());
+	}
+	
+	@PostMapping("/orders/save")
+	public String save(Orders o) {
+		String view = "redirect:/orders/insert";
+		os.insert(o);
+		return view;
+	}
+	
+}
--- src/main/java/com/example/demo/entity/Orders.java
+++ src/main/java/com/example/demo/entity/Orders.java
@@ -1,21 +1,32 @@
-//package com.example.demo.entity;
-//
-//import java.sql.Date;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.Id;
-//import jakarta.persistence.Table;
-//import lombok.Data;
-//
-//@Entity
-//@Data
-//@Table(name="orders")
-//public class Orders {
-//	
-//	@Id
-//	private int orderid;
-//	private int custid;
-//	private int bookid;
-//	private int saleprice;
-//	private Date orderdate;
-//}
+package com.example.demo.entity;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Data
+@Table(name="orders")
+public class Orders {
+	
+	@Id
+	private int orderid;
+	
+	@ManyToOne
+	@JoinColumn(name = "custid", insertable = true, updatable = true)
+	private Customer customer;
+	
+	@ManyToOne
+	@JoinColumn(name = "bookid", insertable = true, updatable = true)
+	private Book book;
+	
+	private int saleprice;
+	private Date orderdate;
+}
