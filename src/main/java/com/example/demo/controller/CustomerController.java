043855b1d306929b8d6379cabb1fa43716edd040
jusunKim <155524612+jusunKim@users.noreply.github.com>
2024-03-22 15:06:46 +0900

    memberJoin수정중[주선]

--- src/main/java/com/example/demo/controller/CustomerController.java
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -3,15 +3,16 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.dao.BookDAO;
 import com.example.demo.dao.CustomerDAO;
+import com.example.demo.entity.Customer;
 import com.example.demo.service.CustomerService;
 
 import lombok.Setter;
 
 @Controller
-@Setter
 public class CustomerController {
 	@Autowired
 	private CustomerService cs;
@@ -19,6 +20,13 @@ public class CustomerController {
 	@GetMapping("/memberJoin")
 	public void joinForm() {
 		
+	}
+	
+	@PostMapping("/memberJoin")
+	public String joinSubmit(Customer c) {
+		String view = "redirect:/listBook어쩌구첫화면?";
+		System.out.println(c.getName());
+		return view;
 	}
 }
 
--- src/main/java/com/example/demo/controller/OrdersController.java
+++ src/main/java/com/example/demo/controller/OrdersController.java
@@ -1,5 +1,5 @@
 package com.example.demo.controller;
-<<<<<<< HEAD
+
 
 
 import java.util.List;
