19ce2b94b0fb78f10415ea61c4fc9b4e021854ed
jusunKim <155524612+jusunKim@users.noreply.github.com>
2024-03-22 12:26:48 +0900

    0322 회원가입, 로그인 html[주선]

--- src/main/java/com/example/demo/controlloer/CustomerController.java
+++ src/main/java/com/example/demo/controlloer/CustomerController.java
@@ -2,6 +2,7 @@ package com.example.demo.controlloer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.demo.dao.BookDAO;
 import com.example.demo.dao.CustomerDAO;
@@ -13,5 +14,15 @@ import lombok.Setter;
 public class CustomerController {
 	@Autowired
 	private CustomerDAO dao; 
+	
+	@GetMapping("/login")
+	public void login() {
+		
+	}
+	
+	@GetMapping("/memberJoin")
+	public void joinForm() {
+		
+	}
 }
 
