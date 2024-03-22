a061c292a059b214793c4814363df2674c79f442
King0-0 <King0-0>
2024-03-22 12:44:46 +0900

    Merge branch 'main' of https://github.com/jusunKim/booktest.git into main

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.demo.dao.BookDAO;
 import com.example.demo.service.BookService;
@@ -13,5 +14,13 @@ import lombok.Setter;
 public class BookController {
 	@Autowired
 	private BookService bs;
+	
+	
+	@GetMapping("/book/detail")
+	public String delete() {
+		String view = "/book/manager/list.html";
+
+		return view;
+	}
 }
 
