d1c8f59dfb869af468b43dc7067e106c2a7fb4f5
King0-0 <King0-0>
2024-03-22 15:12:26 +0900

    bookService,BookController,list.html 수정[수영]

--- src/main/java/com/example/demo/service/BookService.java
+++ src/main/java/com/example/demo/service/BookService.java
@@ -1,16 +1,24 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.dao.BookDAO;
+import com.example.demo.entity.Book;
 
 import lombok.Setter;
 
 @Service
 @Setter
 public class BookService {
-	@Autowired
+	@Autowired 
 	private BookDAO dao;
+	
+	
+	 public List<Book> findAll() {
+		 return dao.findAll();
+	}
 	
 }
