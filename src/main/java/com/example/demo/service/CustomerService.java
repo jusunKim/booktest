64377a75d75f1aef51924ec6c898f5b3c896c084
sangwon0707 <sangwon07@gmail.com>
2024-03-22 16:18:08 +0900

    [상원]

--- src/main/java/com/example/demo/service/CustomerService.java
+++ src/main/java/com/example/demo/service/CustomerService.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,4 +19,7 @@ public class CustomerService {
 	public void insert(Customer c) {
 		dao.save(c);
 	}
-}
+	
+	public List<Customer> findAll(){
+		return dao.findAll();
+	}}
