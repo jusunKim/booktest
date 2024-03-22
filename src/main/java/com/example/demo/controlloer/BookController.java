61641cb76cee3e7a1c6e4722d11d895ff05d7467
King0-0 <King0-0>
2024-03-22 12:22:49 +0900

    [수영]

--- src/main/java/com/example/demo/controlloer/BookController.java
+++ src/main/java/com/example/demo/controlloer/BookController.java
@@ -2,6 +2,7 @@ package com.example.demo.controlloer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.demo.dao.BookDAO;
 
@@ -12,5 +13,12 @@ import lombok.Setter;
 public class BookController {
 	@Autowired
 	private BookDAO dao;
+	
+	@GetMapping("/book/delete")
+	public String delete() {
+		String view = "/book/manager/list.html";
+
+		return view;
+	}
 }
 
