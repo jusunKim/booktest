8917a5717febc3f37033f4bb8664df0e4e78c0a3
jusunKim <155524612+jusunKim@users.noreply.github.com>
2024-03-22 16:55:53 +0900

    CustomerController 최종[주선]

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.service.BookService;
+
+import jakarta.servlet.http.HttpSession;
 import lombok.Setter;
 
 @Controller 
